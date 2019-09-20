5dc57ef4b63e6936f3a68509ee7aaa136e780c30
safonovatanya <safonova.tig@yandex.ru>
2019-09-20 17:11:09 +0300

    two

--- functions/main.js
+++ functions/main.js
@@ -1,31 +1,36 @@
 "use strict"
 //задача1
+
+let a;
+let b;
+let c;
+
 function getSolutions(a,b,c) {
-    let D = b * 2 - 4 * a * c;
-    let x1;
-    let x2;
+    let D = (b ** 2 - 4 * a * c);
     if (D<0) {
         return {D:D}
-    } else if (D==0) {
-        x1 = -b / 2 * a;
-        return { roots : [x1], D:D}
+    } else if (D == 0) {
+        let x1 = (-b / (2 * a))
+        return {roots: x1, D:D}
     } else if (D>0) {
-        x1 = (-b + Math.sqrt(D)) / 2 * a;
-        x2 = (-b - Math.sqrt(D)) / 2 * a;
-        return { roots: [x1 : x2], D : D};
+        let x1 = ((-b + Math.sqrt(D)) / (2 * a));
+        let x2 = ((-b - Math.sqrt(D)) / (2 * a));
+        return {
+            roots: [x1, x2], D:D
+        }
     }
 }
 
-function showSolutionsMessage (a,b,c) {
+function showSolutionsMessage(a,b,c) {
     let result = getSolutions (a,b,c);
-    console.log("Вычислим корни квадратного уравнения ${a}x*x + ${b}x + ${c}");
-    console.log("Значение дискриминанта: + result.D");
+    console.log("Вычислим корни квадратного уравнения ${a}x^2 + ${b}x + ${c} = 0");
+    console.log("Значение дискриминанта: ${result.D}");
     if (result.D < 0) {
-        console.log("Уравнение не имеет вещественных корней")
-    } else if (result.D == 0) {
-        console.log("Уравнение имеет один корень x1 = " + result.roots)
+        console.log("Уравнение не имеет вещественных корней");
+    } else if (result.D) {
+        console.log("Уравнение имеет один корень x1 = ${result.roots[0]}");
     } else if (result.D > 0) {
-        console.log('Уравнение имеет два корня. X₁ = ' + result.roots[0] + ', X₂ = ' + result.roots[1])
+        console.log("Уравнение имеет два корня. X1 = ${result.roots[0]}, x2 = ${result.roots[1]}")
     }
 }
 
@@ -46,39 +51,32 @@ let data = {
     french: [4, 4]
     };
 
-function getAverageScore (data) {
+function getAverageScore (data, scores ) {
     let total = 0;
-    for (i = 0; i < data.lenght; i |+= 1) {
-        total += data [i];
+    for (let i = 0; i < data.lenght; i++) {
+        total += data[i];
     }
     return total / data.lenght;
 }
 
-function getAverageScore(scores){
-    let average = {};
-    let totalcount = 0
-    let totalscore = 0
-    
-    for (key in scores){
-    average[key] = calculateAverage(scores[key])
-    totalscore += calculateAverage(scores[key])
-    totalcount++
-    }
-    average["average"] = totalscore/totalcount
-    return(average)
-    }
+for  (let prop in data) {
+    let value = data [prop];
+    scores = data;
+    scores[prop] = getAverageScoreOfLesson(value);
+}
 
-    console.log(getAverageScore( {
-    algebra: [2, 4, 5, 2, 3, 4],
-    geometry:[2, 4, 5],
-    russian: [3, 3, 4, 5],
-    physics: [5, 5],
-    music: [2, 2, 6],
-    english: [4, 4, 3],
-    poetry: [5, 3, 4],
-    chemistry: [2],
-    french: [4, 4]
-    }))
+let j = 0;
+let sum = 0;
+
+for (let prop in scores) {
+    sum += scores [prop];
+    j++;
+}
+
+scores.average = (sum / j);
+
+console.log(scores);
+getAverageScore (data);
 
     //задача3
     function getPersonData(secretData){
