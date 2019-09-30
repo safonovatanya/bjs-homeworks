daa11d8e1ad9035f885ba503b39d42acc65f0c10
safonovatanya <safonova.tig@yandex.ru>
2019-09-30 19:23:44 +0300

    first

--- async/main.js
+++ async/main.js
@@ -1,25 +1,28 @@
 'use strict';
 
-function setAlarm(time, callback) {
-	return (now) => {
-		now = new Date();
-		let hours = now.getHours();
-		let minutes = now.getMinutes();
-		if (hours < 10) {
-			hours = '0' + hours;
-		} else if (minutes < 10) {
-			minutes = '0' + minutes;
-		}
-		now = hours + ':' + minutes;
-		if (now === time) {
-			callback();
-		}
-	}
-}
-
 function setDailyRhythm(wakeUpTime, bedTime) {
-	setInterval(setAlarm(wakeUpTime, () => alert('Доброе утро, Вася!')), 1000);
-	setInterval(setAlarm(bedTime, () => alert('Доброй ночи, Вася!')), 1000);
-}
 
-setDailyRhythm('07:39', '23:21');
+	const goodMorning = () => alert('Доброе утро!');
+	const goSleep = () => alert('Пора спать!');
+	
+	const wakeUp = setAlarm(wakeUpTime, goodMorning);
+	const goBed = setAlarm(bedTime, goSleep);
+	
+	setInterval(wakeUp, 60000);
+	setInterval(goBed, 60000);
+	}
+	
+	function setAlarm(time, callback) {
+	return function () {
+	const now = new Date();
+	const hoursNow = now.getHours().toString();
+	const minutesNow = now.getMinutes().toString();
+	
+	const [hoursAlarm, minutesAlarm] = time.split(':');
+	
+	if (hoursAlarm === hoursNow && minutesAlarm === minutesNow) {
+	callback();
+	}
+	}
+	}
+setDailyRhythm('17:55', '16.47');	
