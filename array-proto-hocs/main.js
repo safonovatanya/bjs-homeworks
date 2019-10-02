ff98bdb80e1db1856dfc22db9afc9609fd01c805
safonovatanya <safonova.tig@yandex.ru>
2019-10-02 15:53:10 +0300

    4

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,17 +1,8 @@
 'use strict';
 
 function compareArrays(arr1, arr2) {
-	if (arr1.length !== arr2.length) {
-	return false
-	}
-	
-	for (let i = 0; i < arr1.length; i++) {
-	if (arr1[i] !== arr2[i]) {
-	return false
-	}
-	}
-	return true;
-	}
+	return (arr1.length === arr2.length) && (arr1.every(item => arr1.indexOf(item) === arr2.indexOf(item)));
+}
 
 	function memorize(fn, limit) {
 		let results = [];
